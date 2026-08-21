b4c307a2bb8613cc36fbcd24c124cf119eec9f24
Thanushri <thanushrimudhiraj@gmail.com>
2026-08-21 15:10:47 +0530

    Update Maven Java application

--- src/main/java/com/example/my_maven_project/App.java
+++ src/main/java/com/example/my_maven_project/App.java
@@ -1,10 +1,47 @@
 package com.example.my_maven_project;
 
-/**
- * Hello world!
- */
+import java.util.Properties;
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        try {
+
+            Properties properties = new Properties();
+
+            var input = App.class
+                    .getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            properties.load(input);
+
+            System.out.println("App Name: "
+                    + properties.getProperty("app.name"));
+
+            System.out.println("App Version: "
+                    + properties.getProperty("app.version"));
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter first number: ");
+            double num1 = sc.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = sc.nextDouble();
+
+            double sum = num1 + num2;
+
+            System.out.println("Sum: " + sum);
+
+            System.out.println("Patch applied successfully!");
+
+            sc.close();
+            input.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
